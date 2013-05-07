4d4daf189e9601265611425fe1dc91591bc47669
jm2dev <jomarcar@gmail.com>
2013-05-07 22:58:24 +0100

    E2E testing.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -19,4 +19,8 @@ describe('Controller: MainCtrl', function () {
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
+
+  it('should create "phones" model with 3 phones', function () {
+    expect(scope.phones.length).toBe(3);
+  });
 });
