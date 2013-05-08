a37be742514e02e482b9b3de9405197e59543e49
jm2dev <jomarcar@gmail.com>
2013-05-08 14:25:15 +0100

    Sorting by alphabetical order.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,18 +2,17 @@
 
 angular.module('yeomanAngularPhonecatApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
     $scope.phones = [
       {"name": "Nexus S",
-          "snippet": "Fast just got faster with Nexus S."},
+       "snippet": "Fast just got faster with Nexus S.",
+       "age": 0},
       {"name": "Motorola XOOM™ with Wi-Fi",
-          "snippet": "The Next, Next Generation tablet."},
+       "snippet": "The Next, Next Generation tablet.",
+       "age": 1},
       {"name": "MOTOROLA XOOM™",
-          "snippet": "The Next, Next Generation tablet."}
+       "snippet": "The Next, Next Generation tablet.",
+       "age": 2}
     ];
+
+    $scope.orderProp = 'age';
   });
